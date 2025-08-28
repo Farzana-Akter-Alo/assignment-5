a096239a5475dc94ddc55b8c4e553fee17c6edae
Farzana Akter Alo <mstfarzana161@gmail.com>
2025-08-28 13:02:20 -0700

    added card section

---
+++ function.js
@@ -0,0 +1,10 @@
+// function for nav bar
+const heartCountA = document.getElementById("heartcount");
+const coinCountA = document.getElementById("coincount");
+const copyCount = document.getElementById("copycount");
+
+let heartcount = 0;
+let coincount = 100;
+let copycount = 0;
+
+
