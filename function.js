eaa4bcd820736a11fe746ff2494e11290138f29a
Farzana Akter Alo <mstfarzana161@gmail.com>
2025-08-28 22:04:46 -0700

    added call history function

--- function.js
+++ function.js
@@ -33,3 +33,36 @@ cardCopyBtns.forEach((btn) => {
       .catch((err) => console.log(err));
   });
 });
+
+// function for call history
+const historyList = document.getElementById("historyList");
+const clearBtn = document.getElementById("clearHistory");
+
+cardCallBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    if (coincount >= 20) {
+      const confirmCall = confirm(
+        "📞 Call successful!\n Calling\n Your call is connected."
+      );
+      if (confirmCall) {
+        coincount -= 20;
+        coinCountA.textContent = coincount;
+
+        const now = new Date();
+        const time = now.toLocaleTimeString();
+        const li = document.createElement("li");
+        li.className = " mx-5 p-2 bg-white rounded shadow flex justify-between";
+        li.textContent = `Call made at ${time}`;
+        historyList.appendChild(li);
+      }
+    } else {
+      alert(
+        "❌ You don’t have enough coins. You need 20 coins to make a call."
+      );
+    }
+  });
+});
+
+clearBtn.addEventListener("click", () => {
+  historyList.innerHTML = "";
+});
